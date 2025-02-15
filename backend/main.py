a9c4e8be14c576b901a4bff9889784c9f5dfe97c
lpwee <pingwee99@gmail.com>
2025-02-15 19:27:33 +0800

    gemini

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 import uvicorn
+from google import genai
+from dotenv import load_dotenv
+import os
+import json
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
@@ -8,7 +12,9 @@ class VideoRequest(BaseModel):
     # format : str #.mp3 or .mp4
 
 app = FastAPI()
-
+client = genai.Client()
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
 
 @app.get("/")
 async def health() -> dict:
@@ -30,13 +36,22 @@ async def submit(video_link: VideoRequest) -> JSONResponse:
 
     url = video_link.url
 
-    ## put url into cobalt
-    ## obtain mp3 from cobalt
-    ## put mp3 into openai whisper/gemini, get transcript
+
+
+    # cobalt_response = 
+    myfile = client.files.upload(file=cobalt_response.url)
+
+    response = client.models.generate_content(
+    model='gemini-2.0-flash',
+    contents=[
+        'Provide the transcription for this audio clip',
+        myfile,
+    ]
+)
     ## put transcript into llm to organise into recipe
     ## format recipe into json
 
-    content={"video_link" : url} ## temp commented out
+    content={"trasncript" : response.text} ## temp commented out
         # "prep_time" : 
         # cooking_time : response.prep_time
         # total_time : int
